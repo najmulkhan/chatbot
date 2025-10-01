6680ecbeea3e6c2ac8f7e218d9420e7bb7c81b70
Najmul Khan <31945217+najmulkhan@users.noreply.github.com>
2025-10-01 01:59:56 +0000

    deleted file

--- Listmodels.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-    # Configure your API key
-genai.configure(api_key="")
-
-    # List available models
-for m in genai.list_models():
-        print(f"Model: {m.name}, Supported Methods: {m.supported_generation_methods}")
